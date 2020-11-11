e256c796309d9fe836847b3b3a0d219cd1ef1116
Kota1233 <65202792+Kota1233@users.noreply.github.com>
2020-11-11 18:06:43 +0900

    Update-section4.js

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -5,12 +5,20 @@ let cycle;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 120;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  if(keyIsDown(SHIFT)){
+    if (count < 20 || count > 40 && count < 60 || count > 80 && count < 100) size = 40;
+    else size = 60;
+  }
+  else{
+    if (count > 60) size = 40;
+    else size = 60;
+  }
   ellipse(width / 2, height / 2, size);
 }
